bf57fffba247b3165394af74a82e9454728ef682
ayussh19 <cm.b.49ayush.shukla@gmail.com>
2023-02-01 20:52:39 +0530

    railway

--- facerecog/facerecog/wsgi.py
+++ facerecog/facerecog/wsgi.py
@@ -7,10 +7,16 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
+# wsgi.py
 import os
+import sys
+import pymysql # import pymysql
+
+pymysql.install_as_MySQLdb() # call this method before any Django import
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'facerecog.settings')
 
 application = get_wsgi_application()
+
